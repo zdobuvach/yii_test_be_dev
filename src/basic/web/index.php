e01ea5d994b1b20dad5faf65c4434f6859fea50f
Sorkiy <sorkiy@gmail.com>
2021-10-06 18:25:44 +0300

    first commit

---
+++ src/basic/web/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$host = 'mariadb';
+
+// Database use name
+$user = 'uTestYii';
+
+//database user password
+$pass = 'kj7hghkl98GF';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+
+die();
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$config = require __DIR__ . '/../config/web.php';
+
+(new yii\web\Application($config))->run();
---
+++ src/dir1/index.php
@@ -0,0 +1,1 @@
+djkfvjdfdfjn
